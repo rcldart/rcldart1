f1cc8695b5134b73b22c5aedb072e92befb02569
Tim Whiting <tim@whitings.org>
2020-03-20 15:27:01 -0600

    updates

---
+++ bin/gen_ffi.dart
@@ -0,0 +1,18 @@
+import 'package:dshell/dshell.dart';
+import 'package:petitparser/petitparser.dart';
+
+final version = "eloquent";
+
+void main() {
+  final installLoc = "/opt/ros/$version";
+  final includeDir = "$installLoc/include";
+  final rclHeaders = "$includeDir/rcl";
+
+  final rclMainHeader = "$rclHeaders/rcl.h";
+  final command = 'clang -I$includeDir -P -E -nobuiltininc -nostdinc $rclMainHeader';
+  // print(command);
+  final preprocessed = command.toList(runInShell: true, nothrow: false).join("\n");
+  final outputName = 'gen/preprocessed.h';
+  if (exists(outputName)) delete(outputName);
+  outputName.append(preprocessed);
+}
